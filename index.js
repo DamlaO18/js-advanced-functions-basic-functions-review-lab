3833c485d8eb941090c1c294533cd6b477442a20
Damla Osman <damlaaosman@gmail.com>
2022-06-30 20:44:51 +1000

    commit tree 1fe4e1ef0f8e8e40c3f370f2bbb53b09e609613e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+    console.log(`This Saturday, I want to ${activity}!`);
+}
+
+(saturdayFun('bathe my dog'));
+
+function mondayWork(activity='go to the office') {
+    console.log(`This Monday, I will ${activity}.`);
+}
+
+(mondayWork('work from home'));
+
+
+
+
